866e7a5bad38469113a1770213e7e33b47bc7f99
pabhi0901 <pabhishek7333@gmail.com>
2025-11-01 22:42:00 +0530

    xxx

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ app.use(cookieParser())
 app.use(cors({
 
         origin:["http://localhost:5173",
-                "https://ai-app-727y.vercel.app"
+               process.env.frontendLink
         ],
         credentials:true
 
